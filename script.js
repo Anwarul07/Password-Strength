0749829945d9938b65302be5c4db7e92fc370c05
Anwarul07 <mdanwarul064@gmail.com>
2024-05-16 22:18:40 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -12,10 +12,15 @@ pass.addEventListener("input", () => {
     }
     if (pass.value.length < 4) {
         show.innerHTML = "weak";
+        pass.style.borderColor = "#ff5925"
+        msg.style.color = "#ff5925"
     } else if (pass.value.length >= 4 && pass.value.length < 8) {
         show.innerHTML = "medium";
-
+        pass.style.borderColor = "yellow";
+        msg.style.color = "yellow";
     } else if (pass.value.length >= 8) {
         show.innerHTML = "Strong";
+        pass.style.borderColor = "#26d730";
+        msg.style.color = "#26d730";
     }
 });
